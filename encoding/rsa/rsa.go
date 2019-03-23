96fa77a562562958bf8b851676c07b263b5484ab
zfkun <zfkun@msn.com>
2019-03-24 00:33:24 +0800

    rsa update

--- encoding/rsa/rsa.go
+++ encoding/rsa/rsa.go
@@ -4,6 +4,10 @@ import (
     "crypto"
     "crypto/rand"
     "crypto/rsa"
+    "crypto/x509"
+    "encoding/asn1"
+    "encoding/pem"
+    "errors"
 )
 
 func SignPKCS1v15(src, key []byte, hash crypto.Hash) ([]byte, error) {
@@ -42,4 +46,104 @@ func DecryptPKCS1v15(ciphertext, privateKey []byte) ([]byte, error) {
 
 func DecryptPKCS1v15WithKey(ciphertext []byte, priv *rsa.PrivateKey) ([]byte, error) {
     return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+}
+
+// RSA公钥私钥产生
+func GenerateRsaKey(bits int) (pri []byte, priPkcs8 []byte, pub []byte, err error) {
+    // 生成私钥
+    privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+    if err != nil {
+        return
+    }
+    derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+    pri = pem.EncodeToMemory(&pem.Block{
+        Type:  "RSA PRIVATE KEY",
+        Bytes: derStream,
+    })
+    if pri == nil {
+        err = errors.New("pem encode fail")
+        return
+    }
+
+    // 生成PKCS8私钥
+    pk8Stream, err := MarshalPKCS8PrivateKey(derStream)
+    if err != nil {
+        return
+    }
+    priPkcs8 = pem.EncodeToMemory(&pem.Block{
+        Type:  "PRIVATE KEY",
+        Bytes: pk8Stream,
+    })
+    if priPkcs8 == nil {
+        err = errors.New("pem encode fail")
+        return
+    }
+
+    // 生成公钥
+    publicKey := &privateKey.PublicKey
+    derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+    if err != nil {
+        return
+    }
+    // publicBuf := bytes.Buffer{}
+    pub = pem.EncodeToMemory(&pem.Block{
+        Type:  "PUBLIC KEY",
+        Bytes: derPkix,
+    })
+    if pub == nil {
+        err = errors.New("pem encode fail")
+        return
+    }
+
+    return
+}
+
+func GenerateRsaKeyWithTrim(bits int) (pri []byte, priPkcs8 []byte, pub []byte, err error) {
+    pri, priPkcs8, pub, err = GenerateRsaKey(bits)
+    if err == nil {
+        pri = []byte(trimKey(string(pri), pemRsaPrivateKeyPrefix, pemRsaPrivateKeySuffix))
+        priPkcs8 = []byte(trimKey(string(priPkcs8), pemPrivateKeyPrefix, pemPrivateKeySuffix))
+        pub = []byte(trimKey(string(pub), pemPublicKeyPrefix, pemPublicKeySuffix))
+    }
+    return
+}
+
+
+type pkcs8PrivateKey struct {
+    Version             int
+    PrivateKeyAlgorithm []asn1.ObjectIdentifier
+    PrivateKey          []byte
+}
+
+// 由私钥获取PKCS8公钥 这种方式生成的PKCS8与OpenSSL转成的不一样，但是BouncyCastle里可用
+func MarshalPKCS8PrivateKey(key []byte) ([]byte, error) {
+    info := pkcs8PrivateKey{
+        Version: 0,
+        PrivateKeyAlgorithm: make([]asn1.ObjectIdentifier, 1),
+        PrivateKey: key,
+        // PrivateKey: x509.MarshalPKCS1PrivateKey(key), // key : key *rsa.PrivateKey
+    }
+    info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
+
+    k, err := asn1.Marshal(info)
+    if err != nil {
+        return nil, err
+    }
+    return k, nil
+}
+
+// 由私钥获取PKCS8公钥
+func MarshalPKCS8PrivateKey2(key *rsa.PrivateKey) ([]byte, error) {
+    info := pkcs8PrivateKey{
+        Version: 0,
+        PrivateKeyAlgorithm: make([]asn1.ObjectIdentifier, 1),
+        PrivateKey: x509.MarshalPKCS1PrivateKey(key),
+    }
+    info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
+
+    k, err := asn1.Marshal(info)
+    if err != nil {
+        return nil, err
+    }
+    return k, nil
 }
--- encoding/rsa/rsa_test.go
+++ encoding/rsa/rsa_test.go
@@ -1,1 +1,81 @@
 package rsa
+
+import (
+    "encoding/base64"
+    "fmt"
+    "testing"
+)
+
+func TestGenerateRsaKey(t *testing.T) {
+    var pri, priPkcs8, pub, d, v []byte
+    var err error
+
+    d = []byte("hdlgame_01,n-1234567,1234567890")
+
+    pri, priPkcs8, pub, err = GenerateRsaKey(1024)
+    if err != nil {
+        t.Errorf("1024位生成失败: %s", err.Error())
+        t.FailNow()
+    }
+    v, err = EncryptPKCS1v15(d, pub)
+    fmt.Println(string(pri))
+    fmt.Println(string(priPkcs8))
+    fmt.Println(string(pub))
+    fmt.Println(string(v))
+    fmt.Println(base64.StdEncoding.EncodeToString(v))
+    fmt.Println("=======================================")
+
+    pri, priPkcs8, pub, err = GenerateRsaKey(2048)
+    if err != nil {
+        t.Errorf("2048位生成失败: %s", err.Error())
+        t.FailNow()
+    }
+    v, err = EncryptPKCS1v15(d, pub)
+    fmt.Println(string(pri))
+    fmt.Println(string(priPkcs8))
+    fmt.Println(string(pub))
+    fmt.Println(string(v))
+    fmt.Println(base64.StdEncoding.EncodeToString(v))
+    fmt.Println("=======================================")
+}
+
+func TestGenerateRsaKeyWithTrim(t *testing.T) {
+    var pri, priPkcs8, pub, d, v []byte
+    var err error
+
+    d = []byte("hdlgame_01,n-1234567,1234567890")
+
+    pri, priPkcs8, pub, err = GenerateRsaKeyWithTrim(1024)
+    if err != nil {
+        t.Errorf("1024位生成失败: %s", err.Error())
+        t.FailNow()
+    }
+    fmt.Printf("privateKey: %s\n", pri)
+    fmt.Printf("privatePkcs8Key: %s\n", priPkcs8)
+    fmt.Printf("publicKey: %s\n", pub)
+    v, err = EncryptPKCS1v15(d, FormatPublicKey(string(pub)))
+    if err != nil {
+        t.Errorf("encrypt fail: %s", err.Error())
+        t.FailNow()
+    }
+    fmt.Printf("encrypt origin: %s\n", v)
+    fmt.Printf("encrypt base64: %s\n", base64.StdEncoding.EncodeToString(v))
+    fmt.Println("=======================================")
+
+    pri, priPkcs8, pub, err = GenerateRsaKeyWithTrim(2048)
+    if err != nil {
+        t.Errorf("2048位生成失败: %s", err.Error())
+        t.FailNow()
+    }
+    fmt.Printf("privateKey: %s\n", pri)
+    fmt.Printf("privatePkcs8Key: %s\n", priPkcs8)
+    fmt.Printf("publicKey: %s\n", pub)
+    v, err = EncryptPKCS1v15(d, FormatPublicKey(string(pub)))
+    if err != nil {
+        t.Errorf("encrypt fail: %s", err.Error())
+        t.FailNow()
+    }
+    fmt.Printf("encrypt origin: %s\n", v)
+    fmt.Printf("encrypt base64: %s\n", base64.StdEncoding.EncodeToString(v))
+    fmt.Println("=======================================")
+}
