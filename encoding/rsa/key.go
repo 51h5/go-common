943651b78ea1d5a84dd364b991b3a1c524b3a38c
fankun <fankun@51h5.com>
2019-03-24 00:33:24 +0800

    rsa update

--- encoding/rsa/key.go
+++ encoding/rsa/key.go
@@ -5,12 +5,25 @@ import (
     "strings"
 )
 
+const (
+    pemRsaPrivateKeyPrefix = "-----BEGIN RSA PRIVATE KEY-----"
+    pemRsaPrivateKeySuffix = "-----END RSA PRIVATE KEY-----"
+    pemPrivateKeyPrefix    = "-----BEGIN PRIVATE KEY-----"
+    pemPrivateKeySuffix    = "-----BEGIN PRIVATE KEY-----"
+    pemPublicKeyPrefix     = "-----BEGIN PUBLIC KEY-----"
+    pemPublicKeySuffix     = "-----END PUBLIC KEY-----"
+)
+
 func FormatPublicKey(raw string) (result []byte) {
-    return formatKey(raw, "-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----")
+    return formatKey(raw, pemPublicKeyPrefix, pemPublicKeySuffix)
+}
+
+func FormatRsaPrivateKey(raw string) (result []byte) {
+    return formatKey(raw, pemRsaPrivateKeyPrefix, pemRsaPrivateKeySuffix)
 }
 
 func FormatPrivateKey(raw string) (result []byte) {
-    return formatKey(raw, "-----BEGIN RSA PRIVATE KEY-----", "-----END RSA PRIVATE KEY-----")
+    return formatKey(raw, pemPrivateKeyPrefix, pemPrivateKeySuffix)
 }
 
 func formatKey(raw, prefix, suffix string) (result []byte) {
@@ -18,17 +31,12 @@ func formatKey(raw, prefix, suffix string) (result []byte) {
         return nil
     }
 
-    raw = strings.Replace(raw, prefix, "", 1)
-    raw = strings.Replace(raw, suffix, "", 1)
-    raw = strings.Replace(raw, " ", "", -1)
-    raw = strings.Replace(raw, "\n", "", -1)
-    raw = strings.Replace(raw, "\r", "", -1)
-    raw = strings.Replace(raw, "\t", "", -1)
+    raw = trimKey(raw, prefix, suffix)
 
     var ll = 64
     var sl = len(raw)
     var c = sl / ll
-    if sl % ll > 0 {
+    if sl%ll > 0 {
         c = c + 1
     }
 
@@ -46,4 +54,16 @@ func formatKey(raw, prefix, suffix string) (result []byte) {
     }
     buf.WriteString(suffix)
     return buf.Bytes()
+}
+
+func trimKey(raw, prefix, suffix string) string {
+    if raw != "" {
+        raw = strings.Replace(raw, prefix, "", 1)
+        raw = strings.Replace(raw, suffix, "", 1)
+        raw = strings.Replace(raw, " ", "", -1)
+        raw = strings.Replace(raw, "\n", "", -1)
+        raw = strings.Replace(raw, "\r", "", -1)
+        raw = strings.Replace(raw, "\t", "", -1)
+    }
+    return raw
 }
