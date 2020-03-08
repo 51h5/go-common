fd1752d039e7d742042628f056e02520d7a4e20f
fankun <fankun@51h5.com>
2020-03-08 09:59:01 +0800

    dingtalk ua detect support & mod version to 1.13

--- utils/ua.go
+++ utils/ua.go
@@ -3,9 +3,14 @@ package utils
 import "regexp"
 
 var (
-    isTmallRegex = regexp.MustCompile(`(?i:AliApp\((?:TM|TM\-PD)/[0-9.]+\))`)
+    isTmallRegex    = regexp.MustCompile(`(?i:AliApp\((?:TM|TM\-PD)/[0-9.]+\))`)
+    isDingTalkRegex = regexp.MustCompile(`(?i:AliApp\((?:DingTalk)/[0-9.]+\))`)
 )
 
 func IsTmall(ua string) bool {
     return isTmallRegex.MatchString(ua)
+}
+
+func IsDingTalk(ua string) bool {
+    return isDingTalkRegex.MatchString(ua)
 }
--- utils/ua_test.go
+++ utils/ua_test.go
@@ -18,3 +18,20 @@ func TestIsTmall(t *testing.T) {
         t.Fatalf("天猫UA检测失败 (大写)")
     }
 }
+
+func TestIsDingTalk(t *testing.T) {
+    ua := "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 AliApp(DingTalk/4.7.22) com.laiwang.DingTalk/12410922 Channel/201200 language/zh-Hans UT4Aplus/0.0.6 WK"
+    if !IsDingTalk(ua) {
+        t.Fatalf("钉钉UA检测失败 (标准)")
+    }
+
+    ua = "Mobile/5.0 aliapp(dingtalk/4.7.22) com.laiwang.DingTalk/12410922"
+    if !IsDingTalk(ua) {
+        t.Fatalf("钉钉UA检测失败 (小写)")
+    }
+
+    ua = "Mobile/13F69 ALIAPP(DINGTALK/4.7.22) com.laiwang.DingTalk/12410922"
+    if !IsDingTalk(ua) {
+        t.Fatalf("钉钉UA检测失败 (大写)")
+    }
+}
